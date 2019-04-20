074f99a23d30fe1c0f3c3829e1768e338f12ede9
syefir <fate3011@icloud.com>
2019-04-20 15:47:38 +0900

    プログラム追加

---
+++ Assets/CollCreate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollCreate : MonoBehaviour
+{
+
+    [SerializeField]
+    float interval;//間隔
+
+    [SerializeField]
+    GameObject[] collBox;
+
+    //静的定数
+    private const int MAX_CARDS = 64;//複製するオブジェクトの最大数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //オブジェクトを複製する
+        for(var i=0; i < MAX_CARDS; i++)
+        {
+            GameObject newCards = Instantiate(collBox[0]);
+            newCards.transform.position = new Vector3(-4.3f + (i % 8 * interval), 0, -4.3f + (i / 8 * interval));//盤面のマスに合うように間隔をあける
+            newCards.GetComponent<MeshRenderer>().enabled = false;//オブジェクトを見えないようにする
+
+            //スクリプトをアタッチする
+            newCards.AddComponent<CardData>();
+            newCards.AddComponent<SelectPlace>();
+
+            //cardNumberの値を変える
+            newCards.GetComponent<CardData>().cardNumber = i + 1;
+            newCards.GetComponent<SelectPlace>().cardNumber = i + 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Turn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour {
+    public float turn;//ターン数
+    float time;
+    public bool onePush;//
+
+    [SerializeField]
+    GameObject Even, Odd;//奇数、偶数
+    // Use this for initialization
+    void Start()
+    {//初期化
+        time = 0;
+        turn = 1;
+        onePush = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(turn%2==0)
+        {
+            Odd.gameObject.SetActive(false);
+            Even.gameObject.SetActive(true);
+        }
+        else
+        {
+            Even.gameObject.SetActive(false);
+            Odd.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.A)==true || Input.GetKeyDown(KeyCode.S) == true || Input.GetKeyDown(KeyCode.D) == true)
+        {
+            //if (onePush == false)
+            //{
+                turn++;
+
+            //}
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        
+        
+    }
+}
